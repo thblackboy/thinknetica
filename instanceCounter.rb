48e032397bb3577866e3fb15318684e6265ace2f
Alex Alfonso <alfonsoalexandr@gmail.com>
2021-11-22 17:06:16 +0300

    add instances method

--- instanceCounter.rb
+++ instanceCounter.rb
@@ -6,11 +6,13 @@ module InstanceCounter
 
   module ClassMethods
     attr_accessor :instances
+    def instances
+      instances ||= 0
+    end
   end
   module InstanceMethods
     protected
     def register_instance
-      self.class.instances ||= 0
       self.class.instances += 1
     end
   end
